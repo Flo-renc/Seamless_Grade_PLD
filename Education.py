b245ce6d0106ad1b4fb287464e57c17fc3f9a2e0
carine-69 <umugabecarine@gmail.com>
2024-04-01 17:31:27 +0200

    linked education to project1

--- Education.py
+++ Education.py
@@ -1,13 +1,25 @@
 from tkinter import *
 import time
 
+count=0
+text=''
+def slide():
+    global text,count
+    if count==len(s):
+        count=0
+        text=''
+    text=text+s[count]
+    slideLabel.config(text=text)
+    count+=1
+    slideLabel.after (200,slide)
+
 def clock():
     date = time.strftime('%d/%m/%Y')
     current_time = time.strftime('%H:%M:%S')
-    datetimeLabel.config(text=f"{date} {current_time}")  # Update label text
+    datetimeLabel.config(text=f"   date: {date}\nTime:{current_time}")  # Update label text
     window.after(1000, clock)  # Update the clock every second
 
-window = Tk()
+window =Tk()
 window.geometry('1244x700+0+0')
 window.title('Student Management System')
 
@@ -15,5 +27,10 @@ datetimeLabel = Label(window, font=('times new Roman', 12, 'bold'))
 datetimeLabel.place(x=5, y=5)
 
 clock()  # Start the clock function
-
+s='Student Management System'
+slideLabel=Label(window,text=s,font=('Arial',28,'bold'))
+slideLabel.place(x=200,y=0)
+slide()
+connectButton=Button(window,text='connect database')
+connectButton.place(x=980,y=0)
 window.mainloop()
