2cfebc463f748452e7c4bd3b0f466720edacf4dd
Carine-69 <155974482+Carine-69@users.noreply.github.com>
2024-04-01 11:21:01 +0200

    updating date current time

---
+++ Education.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import time
+
+def clock():
+    date = time.strftime('%d/%m/%Y')
+    current_time = time.strftime('%H:%M:%S')
+    datetimeLabel.config(text=f"{date} {current_time}")  # Update label text
+    window.after(1000, clock)  # Update the clock every second
+
+window = Tk()
+window.geometry('1244x700+0+0')
+window.title('Student Management System')
+
+datetimeLabel = Label(window, font=('times new Roman', 12, 'bold')) 
+datetimeLabel.place(x=5, y=5)
+
+clock()  # Start the clock function
+
+window.mainloop()
